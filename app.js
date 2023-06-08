68255b743495a01aed8b96eeed735d107f56ffd6
eiliyahs <101492184+eiliyahs@users.noreply.github.com>
2023-06-08 15:33:26 -0400

    lab 2.4.1

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const app = express();
 const port = 3000;
 const logger = require("morgan");
 
+const DEBUG = true;
+
+const db = require('./db/db_connection');
+
+const read_skinCareList_all_sql = `
+    SELECT skinCareId, name, step, skinCareTypeName, skinCare.skinCareTypeId as skinCareTypeId, cost
+    FROM skinCare
+    JOIN skinCareType
+        ON skinCare.skinCareTypeId = skinCareType.skinCareTypeId
+    ORDER BY skinCare.step 
+`
+
+const read_skinCareList_detail_sql = `
+    SELECT skinCareId, name, step, skinCareTypeName, skinCare.skinCareTypeId as skinCareTypeId, cost, description
+    FROM skinCare
+    JOIN skinCareType
+        ON skinCare.skinCareTypeId = skinCareType.skinCareTypeId
+    WHERE skinCareId = ?
+`
 
 // start the server
 app.listen( port, () => {
@@ -23,12 +42,28 @@ app.get( "/", ( req, res ) => {
 
 // define a route for the list page
 app.get( "/list", ( req, res ) => {
-    res.sendFile(__dirname + "/views/list.html");
+    db.execute(read_skinCareList_all_sql, (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else
+            res.send(results);
+    });
 } );
 
 // define a route for the edit page
-app.get( "/list/edit", ( req, res ) => {
-    res.sendFile(__dirname + "/views/edit.html");
+app.get( "/list/:id", ( req, res ) => {
+    db.execute(read_skinCareList_detail_sql, [req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else if (results.length == 0)
+            res.status(404).send(`No assignment found with id = "${req.params.id}"` ); // NOT FOUND
+        else
+            res.send(results[0]); // results is still an array
+    });
 } );
 
 // define a route for the recs page
