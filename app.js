9d9e1d1e158c9edafdac1b0e17543a4c88fca6fd
eiliyahs <101492184+eiliyahs@users.noreply.github.com>
2023-06-22 16:39:16 -0400

    lab 4 p2

--- app.js
+++ app.js
@@ -29,16 +29,23 @@ app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
 } );
 
+// Configure Express to use EJS
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
+
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
+
+// Configure Express to parse URL-encoded POST request bodies (traditional forms)
+app.use( express.urlencoded({ extended: false }) );
 
 // define middleware that serves static resources in the public directory
 app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile(__dirname + "/views/index.html");
-} );
+    res.render('index');
+});
 
 // define a route for the list page
 app.get( "/list", ( req, res ) => {
@@ -47,8 +54,10 @@ app.get( "/list", ( req, res ) => {
             console.log(error ? error : results);
         if (error)
             res.status(500).send(error); //Internal Server Error
-        else
-            res.send(results);
+        else {
+            let data = { sclist : results };
+            res.render('list', data);
+        }
     });
 } );
 
@@ -58,11 +67,13 @@ app.get( "/list/:id", ( req, res ) => {
         if (DEBUG)
             console.log(error ? error : results);
         if (error)
-            res.status(500).send(error); //Internal Server Error
+            res.status(500).send(error); 
         else if (results.length == 0)
-            res.status(404).send(`No assignment found with id = "${req.params.id}"` ); // NOT FOUND
-        else
-            res.send(results[0]); // results is still an array
+            res.status(404).send(`No assignment found with id = "${req.params.id}"` ); 
+            else {
+                let data = {sc: results[0]}; // results is still an array, get first (only) element
+                res.render('edit', data); 
+            }
     });
 } );
 
@@ -70,3 +81,69 @@ app.get( "/list/:id", ( req, res ) => {
 app.get( "/recs", ( req, res ) => {
     res.sendFile(__dirname + "/views/recs.html");
 } );
+
+
+// define a route for assignment DELETE
+const delete_skincare_sql = `
+    DELETE 
+    FROM
+        skinCare
+    WHERE
+        skinCareId = ?
+`
+app.get("/list/:id/delete", ( req, res ) => {
+    db.execute(delete_skincare_sql, [req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect("/list");
+        }
+    });
+});
+
+// define a route for assignment CREATE
+const create_skinCare_sql = `
+    INSERT INTO skinCare 
+        (name, step, skinCareTypeId, cost) 
+    VALUES 
+        (?, ?, ?, ?);
+`
+
+app.post("/list", ( req, res ) => {
+    db.execute(create_skinCare_sql, [req.body.name, req.body.step, req.body.type, req.body.cost], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/list/${results.insertId}`);
+        }
+    });
+});
+
+const update_skinCare_sql = `
+    UPDATE
+        skinCare
+    SET
+        name = ?,
+        step = ?,
+        skinCareTypeId = ?,
+        cost = ?,
+        description = ?
+    WHERE
+        skinCareId = ?
+`
+
+app.post("/list/:id", ( req, res ) => {
+    db.execute(update_skinCare_sql, [req.body.name, req.body.step, req.body.type, req.body.cost, req.body.description, req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/list/${req.params.id}`);
+        }
+    });
+});
